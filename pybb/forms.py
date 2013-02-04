5fa7eb72c7278c295ef8ae8c67b86c3fcb19c809
Aaron Ogle <atogle@gmail.com>
2013-02-04 12:28:11 -0500

    Add fence back to WatchAreaForm fields

--- pybb/forms.py
+++ pybb/forms.py
@@ -64,7 +64,7 @@ class WatchAreaForm(forms.ModelForm):
 
     class Meta(object):
         model = WatchArea
-        fields = ('name', )
+        fields = ('name', 'fence')
 
     def __init__(self, user, *args, **kwargs):
         self.user = user
